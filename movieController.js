7dab0ac1f72790ab6b1fd9694e30335a794f1fc7
KimYongKwon <dydrnjs518@gmail.com>
2020-09-25 16:54:56 +0900

    Update Super Controller part2

--- movieController.js
+++ movieController.js
@@ -8,12 +8,42 @@ import {
 
 export const home = (req, res) => {
   const movies = getMovies();
-  res.render("movies", { pageTitle: "Movies!", title: "Movies!", movies });
+  if (movies.length !== 0) {
+    res.render("movies", { pageTitle: "Movies!", title: "Movies!", movies });
+  } else {
+    res.render("404");
+  }
 };
 
 export const movieDetail = (req, res) => {
-  const movie = getMovieById(req.params.id);
-  res.render("detail", { pageTitle: movie.title, movie });
+  const {
+    params: { id },
+  } = req;
+  const movie = getMovieById(id);
+  if (movie) {
+    res.render("detail", { pageTitle: movie.title, movie });
+  } else {
+    res.render("404");
+  }
 };
 
-export const filterMovie = (req, res) => {};
+export const filterMovie = (req, res) => {
+  let movies = [];
+  if (req.query.rating != null) {
+    movies = getMovieByMinimumRating(parseFloat(req.query.rating));
+    res.render("movies", {
+      pageTitle: `Search(rating${req.query.rating})`,
+      title: `Searcing by rating : ${req.query.rating}`,
+      movies,
+    });
+  } else if (req.query.year != null) {
+    movies = getMovieByMinimumYear(parseInt(req.query.year, 10));
+    res.render("movies", {
+      pageTitle: `Search(year${req.query.year})`,
+      title: `Searcing by year : ${req.query.year}`,
+      movies,
+    });
+  } else {
+    res.render("404");
+  }
+};
